08aa241fbefb9c50edd07806a01453d0cd61787e
TheTemportalist <the.country.gamer@gmail.com>
2015-02-07 13:56:32 -0500

    -: Uncommenting

--- src/main/java/com/temportalist/compression/client/ProxyClient.scala
+++ src/main/java/com/temportalist/compression/client/ProxyClient.scala
@@ -3,12 +3,15 @@ package com.temportalist.compression.client
 import com.temportalist.compression.client.model.ModelCompressed
 import com.temportalist.compression.common.ProxyCommon
 import com.temportalist.compression.common.init.CBlocks
+import net.minecraft.block.state.IBlockState
+import net.minecraft.client.renderer.block.statemap.StateMapperBase
 import net.minecraft.client.resources.model.ModelResourceLocation
 import net.minecraft.entity.player.EntityPlayer
+import net.minecraft.item.Item
 import net.minecraft.tileentity.TileEntity
 import net.minecraft.world.World
 import net.minecraftforge.client.event.ModelBakeEvent
-import net.minecraftforge.client.model.ISmartBlockModel
+import net.minecraftforge.client.model.{ModelLoader, ISmartBlockModel}
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 
 /**
@@ -26,7 +29,6 @@ class ProxyClient extends ProxyCommon {
 
 	override def registerRender(): Unit = {
 
-		/*
 		ModelLoader.setCustomModelResourceLocation(
 			Item.getItemFromBlock(CBlocks.compressed), 0,
 			compressedItem
@@ -35,7 +37,6 @@ class ProxyClient extends ProxyCommon {
 			override def getModelResourceLocation(iBlockState: IBlockState): ModelResourceLocation =
 				compressedBlock
 		})
-		*/
 
 	}
 
@@ -43,7 +44,7 @@ class ProxyClient extends ProxyCommon {
 	def bake(event: ModelBakeEvent): Unit = {
 		val model: ISmartBlockModel = new ModelCompressed()
 		event.modelRegistry.putObject(this.compressedBlock, model)
-		//event.modelRegistry.putObject(this.compressedItem, model)
+		event.modelRegistry.putObject(this.compressedItem, model)
 	}
 
 }
