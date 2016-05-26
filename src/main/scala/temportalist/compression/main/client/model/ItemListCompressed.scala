fee5fe6ed5d1dedf0816806e5ab55115232f2618
TheTemportalist <the.country.gamer@gmail.com>
2016-05-26 10:13:32 -0400

    Add a more comprehensive error message for invalid item renders

--- src/main/scala/temportalist/compression/main/client/model/ItemListCompressed.scala
+++ src/main/scala/temportalist/compression/main/client/model/ItemListCompressed.scala
@@ -8,15 +8,15 @@ import net.minecraft.client.Minecraft
 import net.minecraft.client.renderer.block.model.SimpleBakedModel.Builder
 import net.minecraft.client.renderer.block.model._
 import net.minecraft.client.renderer.texture.TextureAtlasSprite
-import net.minecraft.client.renderer.vertex.DefaultVertexFormats
 import net.minecraft.entity.EntityLivingBase
 import net.minecraft.item.{ItemBlock, ItemStack}
 import net.minecraft.util.EnumFacing
 import net.minecraft.util.math.BlockPos
 import net.minecraft.world.World
-import net.minecraftforge.client.model.ItemLayerModel
+import temportalist.compression.main.common.Compression
 import temportalist.compression.main.common.init.Compressed
 import temportalist.compression.main.common.lib.EnumTier
+import temportalist.origin.api.common.helper.Names
 
 /**
   *
@@ -65,19 +65,27 @@ class ItemListCompressed(private val overlays: Array[TextureAtlasSprite])
 			override def getQuads(state: IBlockState,
 					side: EnumFacing,
 					rand: Long): util.List[BakedQuad] = {
-				val quadList = new util.ArrayList[BakedQuad]()
-				quadList.addAll(sampleModel.getQuads(state, side, rand))
 
-				if (overlay != null) {
-					try {
+				val quadList = new util.ArrayList[BakedQuad]()
+
+				try {
+					quadList.addAll(sampleModel.getQuads(state, side, rand))
+					if (overlay != null) {
 						val overlayModel = new Builder(
 							state, sampleModel, overlay, BlockPos.ORIGIN).makeBakedModel()
 						quadList.addAll(overlayModel.getQuads(state, side, rand))
 					}
-					catch {
-						case e: Exception =>
-							e.printStackTrace()
-					}
+				}
+				catch {
+					case e: Exception =>
+						Compression.log("Error merging render models. " +
+								"Please report this to https://github.com/TheTemportalist/Compression/issues. " +
+								"As a temporary fix, you can consider adding \'" +
+								Names.getName(stack, hasID = true, hasMeta = false) +
+								"\' or \'" +
+								Names.getName(stack, hasID = true, hasMeta = true) +
+								"\' to the blacklist configuration option.")
+						e.printStackTrace()
 				}
 
 				quadList
